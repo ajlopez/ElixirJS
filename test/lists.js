ab2767371a6d9c371b760f6e2499795a725e6ee7
ajlopez <webmaster@ajlopez.com>
2015-07-19 15:51:57 -0300

    Create list

---
+++ test/lists.js
@@ -0,0 +1,13 @@
+
+var lists = require('../lib/lists');
+
+exports['create list'] = function (test) {
+    var list = lists.list(1, lists.list(2, null));
+    
+    test.ok(list);
+    test.equal(typeof list, 'object');
+    test.equal(list.head(), 1);
+    test.equal(list.tail().head(), 2);
+    test.equal(list.tail().tail(), null);
+    test.equal(list.length(), 2);
+}
