9363f020851609c59eb5181aaf13799cf7e3cb5d
ajlopez <webmaster@ajlopez.com>
2015-07-22 07:10:31 -0300

    Context set and get

--- test/contexts.js
+++ test/contexts.js
@@ -9,3 +9,12 @@ exports['get undefined variable'] = function (test) {
     test.strictEqual(context.get("foo"), undefined);
 }
 
+exports['set and get variable'] = function (test) {
+    var context = contexts.context();
+    
+    test.ok(context);
+    test.equal(typeof context, 'object');
+    context.set("foo", 42);
+    test.equal(context.get("foo"), 42);
+}
+
