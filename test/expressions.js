022a0e636bed36701f2eabcc65706bf932ea4cf5
ajlopez <webmaster@ajlopez.com>
2015-07-25 08:33:03 -0300

    Evaluate variable in context

--- test/expressions.js
+++ test/expressions.js
@@ -1,5 +1,6 @@
 
 var expressions = require('../lib/expressions');
+var contexts = require('../lib/contexts');
 
 exports['create and evaluate integer constant'] = function (test) {
     var result = expressions.constant(42);
@@ -7,5 +8,16 @@ exports['create and evaluate integer constant'] = function (test) {
     test.ok(result);
     test.equal(typeof result, 'object');
     test.equal(result.evaluate(), 42);
+};
+
+exports['create and evaluate variable in context'] = function (test) {
+    var result = expressions.variable("n");
+    var ctx = contexts.context();
+    
+    ctx.set("n", 42);
+    
+    test.ok(result);
+    test.equal(typeof result, 'object');
+    test.equal(result.evaluate(ctx), 42);
 };
 
