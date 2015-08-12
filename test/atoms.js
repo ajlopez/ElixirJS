a0a31ea8ac11ce1176d958dbb784c68f8f6bdac8
ajlopez <webmaster@ajlopez.com>
2015-08-12 06:29:52 -0300

    Create atom

---
+++ test/atoms.js
@@ -0,0 +1,11 @@
+
+var atoms = require('../lib/atoms');
+
+exports['create atom'] = function (test) {
+    var atom = atoms.atom("foo");
+    
+    test.ok(atom);
+    test.equal(typeof atom, 'object');
+    test.equal(atom.toString(), ":foo");
+}
+
