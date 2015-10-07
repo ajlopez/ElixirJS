f5020f907b4b2035043531d9a3646f8a99f40c34
ajlopez <webmaster@ajlopez.com>
2015-10-07 05:23:06 -0300

    Lexer get string

--- test/lexers.js
+++ test/lexers.js
@@ -82,3 +82,15 @@ exports['get arithmethic operators'] = function (test) {
     test.equal(lexer.nextToken(), null);
 };
 
+exports['get string'] = function (test) {
+    var lexer = lexers.lexer('"foo"');
+    
+    var token = lexer.nextToken();
+    
+    test.ok(token);
+    test.equal(token.value, "foo");
+    test.equal(token.type, TokenType.String);
+    
+    test.equal(lexer.nextToken(), null);
+};
+
