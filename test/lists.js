e5c1ca90a02ef598a4c4572f2a010626797829a5
ajlopez <webmaster@ajlopez.com>
2015-10-12 19:21:05 -0300

    List to string

--- test/lists.js
+++ test/lists.js
@@ -15,7 +15,19 @@ exports['create list'] = function (test) {
     test.equal(list.isAtom(), false);
     test.equal(list.isVariable(), false);
     test.equal(list.isTuple(), false);
-}
+};
+
+exports['list to string'] = function (test) {
+    var list = lists.list(1, lists.list(2, null));
+    
+    test.equal(list.toString(), "[ 1, 2 ]");
+};
+
+exports['empty list to string'] = function (test) {
+    var list = lists.list();
+    
+    test.equal(list.toString(), "[]");
+};
 
 exports['create list from array'] = function (test) {
     var list = lists.create([1, 2, 3]);
@@ -27,7 +39,7 @@ exports['create list from array'] = function (test) {
     test.equal(list.tail().tail().head(), 3);
     test.equal(list.tail().tail().tail(), null);
     test.equal(list.length(), 3);
-}
+};
 
 exports['get elements'] = function (test) {
     var list = lists.create([1, 2, 3]);
