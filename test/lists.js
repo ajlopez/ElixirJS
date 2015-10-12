7a5e66999396d7508ad2f0a919fdc15a40b99266
ajlopez <webmaster@ajlopez.com>
2015-10-12 19:11:54 -0300

    Create list from array

--- test/lists.js
+++ test/lists.js
@@ -17,6 +17,18 @@ exports['create list'] = function (test) {
     test.equal(list.isTuple(), false);
 }
 
+exports['create list from array'] = function (test) {
+    var list = lists.create([1, 2, 3]);
+    
+    test.ok(list);
+    test.equal(typeof list, 'object');
+    test.equal(list.head(), 1);
+    test.equal(list.tail().head(), 2);
+    test.equal(list.tail().tail().head(), 3);
+    test.equal(list.tail().tail().tail(), null);
+    test.equal(list.length(), 3);
+}
+
 exports['create empty list'] = function (test) {
     var list = lists.list();
     
