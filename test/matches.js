33993db76eabf38bf406d811bf7d4e35d40c7429
ajlopez <webmaster@ajlopez.com>
2015-10-12 08:57:24 -0300

    Match variable

--- test/matches.js
+++ test/matches.js
@@ -1,5 +1,7 @@
 
 var matches = require('../lib/matches');
+var variables = require('../lib/variables');
+var contexts = require('../lib/contexts');
 
 exports['match integers'] = function (test) {
     test.ok(matches.match(1, 1, null));
@@ -9,5 +11,12 @@ exports['match integers'] = function (test) {
 exports['match strings'] = function (test) {
     test.ok(matches.match("foo", "foo", null));
     test.equal(matches.match("foo", "bar", null), false);
+};
+
+exports['match variable'] = function (test) {
+    var vara = variables.variable('a');
+    var ctx = contexts.context();
+    test.ok(matches.match(vara, 42, ctx));
+    test.equal(ctx.get('a'), 42);
 };
 
