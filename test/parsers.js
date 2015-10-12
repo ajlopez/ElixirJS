b4268a1b2918ef91300c0398fa71a23a54ee8d68
ajlopez <webmaster@ajlopez.com>
2015-10-12 18:23:59 -0300

    Parse and evaluate string

--- test/parsers.js
+++ test/parsers.js
@@ -13,3 +13,15 @@ exports['parse and evaluate integer'] = function (test) {
     test.equal(parser.parseExpression(), null);
 };
 
+exports['parse and evaluate string'] = function (test) {
+    var parser = parsers.parser('"foo"');
+    
+    var result = parser.parseExpression();
+    
+    test.ok(result);
+    
+    test.strictEqual(result.evaluate(null), "foo");
+    
+    test.equal(parser.parseExpression(), null);
+};
+
