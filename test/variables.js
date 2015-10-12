b00037b55413d1367807ab43fa381582ca05b9df
ajlopez <webmaster@ajlopez.com>
2015-10-12 07:55:41 -0300

    Predicate functions in Variable

--- test/variables.js
+++ test/variables.js
@@ -8,5 +8,9 @@ exports['create variable'] = function (test) {
     test.equal(typeof variable, 'object');
     test.equal(variable.toString(), "foo");
     test.equal(variable.name(), "foo");
+    test.ok(variable.isVariable());
+    test.equal(variable.isAtom(), false);
+    test.equal(variable.isList(), false);
+    test.equal(variable.isTuple(), false);
 }
 
