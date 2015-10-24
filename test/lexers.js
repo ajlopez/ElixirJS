c598b18f69d2dcac14ee6426d0a523e602cc6827
ajlopez <webmaster@ajlopez.com>
2015-10-24 08:15:54 -0300

    Lexer gets string concatenation operator

--- test/lexers.js
+++ test/lexers.js
@@ -132,6 +132,18 @@ exports['get list operators'] = function (test) {
     test.equal(lexer.nextToken(), null);
 };
 
+exports['get string concatenation operator'] = function (test) {
+    var lexer = lexers.lexer('<>');
+    
+    var token = lexer.nextToken();
+    
+    test.ok(token);
+    test.equal(token.value, "<>");
+    test.equal(token.type, TokenType.Operator);
+    
+    test.equal(lexer.nextToken(), null);
+};
+
 exports['get strict boolean operators'] = function (test) {
     var lexer = lexers.lexer('not and or');
     
