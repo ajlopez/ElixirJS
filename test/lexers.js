d50690e339743c3bbd33f60da7380de9308e83c0
ajlopez <webmaster@ajlopez.com>
2015-11-15 08:28:45 -0300

    Lexer gets binary list delimiters

--- test/lexers.js
+++ test/lexers.js
@@ -292,3 +292,16 @@ exports['get first delimiters'] = function (test) {
     test.equal(lexer.nextToken(), null);
 };
 
+exports['get binary list delimiter'] = function (test) {
+    var lexer = lexers.lexer('<< >>');
+    
+    ["<<", ">>"].forEach(function (value) {
+        var token = lexer.nextToken();
+        
+        test.ok(token);
+        test.equal(token.value, value);
+        test.equal(token.type, TokenType.Delimiter);
+    });
+    
+    test.equal(lexer.nextToken(), null);
+};
