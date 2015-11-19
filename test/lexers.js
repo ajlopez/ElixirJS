2151e68dc467707aa5538bf2bfbf1bb8ff08e49d
ajlopez <webmaster@ajlopez.com>
2015-11-19 06:39:28 -0300

    Lexer gets right arrow as operator

--- test/lexers.js
+++ test/lexers.js
@@ -377,3 +377,16 @@ exports['get binary list delimiter'] = function (test) {
     
     test.equal(lexer.nextToken(), null);
 };
+
+exports['get right arrow as operator'] = function (test) {
+    var lexer = lexers.lexer('=>');
+    
+    var token = lexer.nextToken();
+    
+    test.ok(token);
+    test.equal(token.value, "=>");
+    test.equal(token.type, TokenType.Operator);
+    
+    test.equal(lexer.nextToken(), null);
+};
+
