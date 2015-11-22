8b7d1a1701648695216c4fe5f6f4a3c1e4b51ae2
ajlopez <webmaster@ajlopez.com>
2015-11-22 06:39:56 -0300

    Lexer gets name ending with exclamation mark

--- test/lexers.js
+++ test/lexers.js
@@ -38,6 +38,18 @@ exports['get name with question mark'] = function (test) {
     test.equal(lexer.nextToken(), null);
 };
 
+exports['get name with exclamation mark'] = function (test) {
+    var lexer = lexers.lexer('foo!');
+    
+    var token = lexer.nextToken();
+
+    test.ok(token);
+    test.equal(token.value, 'foo!');
+    test.equal(token.type, TokenType.Name);
+    
+    test.equal(lexer.nextToken(), null);
+};
+
 exports['get integer'] = function (test) {
     var lexer = lexers.lexer('123');
     
