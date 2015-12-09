719bc3d75774f1ee5833c758b2294bd6405e94a4
ajlopez <webmaster@ajlopez.com>
2015-12-09 06:52:32 -0300

    member? function in enum object

--- test/objs/enum.js
+++ test/objs/enum.js
@@ -6,3 +6,11 @@ exports['reverse function on array'] = function (test) {
     test.equal(typeof enumobj.reverse, 'function');
     test.deepEqual(enumobj.reverse([1, 2, 3]), [3, 2, 1]);
 };
+
+exports['member function on array'] = function (test) {
+    test.ok(enumobj['member?']);
+    test.equal(typeof enumobj['member?'], 'function');
+    test.strictEqual(enumobj['member?']([1, 2, 3], 1), true);
+    test.strictEqual(enumobj['member?']([1, 2, 3], 2), true);
+    test.strictEqual(enumobj['member?']([1, 2, 3], 4), false);
+};
