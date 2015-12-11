4566a3d24c35330ffa18a0cad8f316fca9950895
ajlopez <webmaster@ajlopez.com>
2015-12-11 06:26:06 -0300

    is_boolean function

--- test/objs/main.js
+++ test/objs/main.js
@@ -6,3 +6,14 @@ exports['length function on array'] = function (test) {
     test.equal(typeof main.length, 'function');
     test.equal(main.length([1, 2, 3]), 3);
 };
+
+exports['is_boolean function'] = function (test) {
+    test.ok(main.is_boolean);
+    test.equal(typeof main.is_boolean, 'function');
+    test.strictEqual(main.is_boolean(null), false);
+    test.strictEqual(main.is_boolean(42), false);
+    test.strictEqual(main.is_boolean("foo"), false);
+    test.strictEqual(main.is_boolean(3.1416), false);
+    test.strictEqual(main.is_boolean(false), true);
+    test.strictEqual(main.is_boolean(true), true);
+};
