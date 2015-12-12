9d106539421b04b35610df79acdc4e115b31e807
ajlopez <webmaster@ajlopez.com>
2015-12-12 06:10:30 -0300

    is_atom function

--- test/objs/main.js
+++ test/objs/main.js
@@ -1,5 +1,6 @@
 
 var main = require('../../lib/objs/main');
+var atoms = require('../../lib/atoms');
 
 exports['length function on array'] = function (test) {
     test.ok(main.length);
@@ -16,4 +17,20 @@ exports['is_boolean function'] = function (test) {
     test.strictEqual(main.is_boolean(3.1416), false);
     test.strictEqual(main.is_boolean(false), true);
     test.strictEqual(main.is_boolean(true), true);
+    test.strictEqual(main.is_boolean(atoms.atom('false')), true);
+    test.strictEqual(main.is_boolean(atoms.atom('true')), true);
 };
+
+exports['is_atom function'] = function (test) {
+    test.ok(main.is_atom);
+    test.equal(typeof main.is_atom, 'function');
+    test.strictEqual(main.is_atom(null), false);
+    test.strictEqual(main.is_atom(42), false);
+    test.strictEqual(main.is_atom("foo"), false);
+    test.strictEqual(main.is_atom(3.1416), false);
+    test.strictEqual(main.is_atom([1, 2, 3]), false);
+    test.strictEqual(main.is_atom(false), true);
+    test.strictEqual(main.is_atom(true), true);
+    test.strictEqual(main.is_atom(atoms.atom('foo')), true);
+};
+
