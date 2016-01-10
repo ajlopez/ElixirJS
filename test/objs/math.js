071cfb0aa9e64f273f6214a7b3aff7784af53d40
ajlopez <webmaster@ajlopez.com>
2016-01-10 18:32:54 -0300

    math sin function

--- test/objs/math.js
+++ test/objs/math.js
@@ -5,3 +5,8 @@ exports['Pi value'] = function (test) {
     test.equal(math.pi, Math.PI);
 }
 
+exports['sine function'] = function (test) {
+    test.equal(math.sin(0), Math.sin(0));
+    test.equal(math.sin(1), Math.sin(1));
+    test.equal(math.sin(math.pi), Math.sin(Math.PI));
+}
