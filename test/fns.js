b6bd3e07f5707fb14a9b538eeb869f58eb1f21fa
ajlopez <webmaster@ajlopez.com>
2016-02-01 14:04:34 -0300

    function with one expression

--- test/fns.js
+++ test/fns.js
@@ -1,6 +1,7 @@
 
 var fns = require('../lib/fns');
 var contexts = require('../lib/contexts');
+var expressions = require('../lib/expressions');
 
 exports['create and evaluate empty function'] = function (test) {
     var fn = fns.fn('foo', [], []);
@@ -13,3 +14,16 @@ exports['create and evaluate empty function'] = function (test) {
     
     test.equal(result, null);
 }
+
+exports['function with one expression'] = function (test) {
+    var fn = fns.fn('foo', [], [expressions.constant(42)]);
+    var ctx = contexts.context();
+    
+    test.equal(fn.name(), 'foo');
+    test.equal(fn.arity(), 0);
+    
+    var result = fn.evaluate(ctx, []);
+    
+    test.equal(result, 42);
+}
+
