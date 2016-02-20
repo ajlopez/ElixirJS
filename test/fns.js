c5e759ed6804a498a596c0d27ce7f77b5fc64d1d
ajlopez <webmaster@ajlopez.com>
2016-02-20 11:35:10 -0300

    Redesign fns to use original context for evaluation

--- test/fns.js
+++ test/fns.js
@@ -4,37 +4,37 @@ var contexts = require('../lib/contexts');
 var expressions = require('../lib/expressions');
 
 exports['create and evaluate empty function'] = function (test) {
-    var fn = fns.fn('foo', [], []);
     var ctx = contexts.context();
+    var fn = fns.fn('foo', [], [], ctx);
     
     test.equal(fn.name(), 'foo');
     test.equal(fn.arity(), 0);
     
-    var result = fn.evaluate(ctx, []);
+    var result = fn.evaluate([]);
     
     test.equal(result, null);
 }
 
 exports['function with one expression'] = function (test) {
-    var fn = fns.fn('foo', [], [expressions.constant(42)]);
     var ctx = contexts.context();
+    var fn = fns.fn('foo', [], [expressions.constant(42)], ctx);
     
     test.equal(fn.name(), 'foo');
     test.equal(fn.arity(), 0);
     
-    var result = fn.evaluate(ctx, []);
+    var result = fn.evaluate([]);
     
     test.equal(result, 42);
 }
 
 exports['function with one expression referencing argument'] = function (test) {
-    var fn = fns.fn('foo', ["a"], [expressions.variable("a")]);
     var ctx = contexts.context();
+    var fn = fns.fn('foo', ["a"], [expressions.variable("a")], ctx);
     
     test.equal(fn.name(), 'foo');
     test.equal(fn.arity(), 1);
     
-    var result = fn.evaluate(ctx, [42]);
+    var result = fn.evaluate([42]);
     
     test.equal(result, 42);
 }
